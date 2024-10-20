6faed86ae189f59329076df2237c86d9d556ec84
Andrushkon <andrushcon@gmail.com>
2024-10-20 10:41:02 +0100

    First version

--- code.js
+++ code.js
@@ -1,25 +1,17 @@
-const todaysword=async function (){
-   let date=new Date()
+let gettodaysword=function(){
+    let date=new Date()
+    let day=date.getDate()
     let month=date.getMonth()+1
     let year=date.getFullYear()
     let url=`https://www.nytimes.com/svc/wordle/v2/${year}-${month}-${day}.json`
-    alert(url)
-        try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-          const json = await response.json();
-          alert(json)
-          return json.word
-                } catch (error) {
-          alert(error.message);
-        }
-}
+      
+      
+    }
+let todaysword=null
 const menu=document.getElementById("menu")
 const game=document.getElementById("game")
 const results=document.getElementById("result")
-const listofwords=["MORON"]
+const listofwords= ["about", "above","abuse", "actor", "acute", "admit", "adopt", "adult", "after", "again", "agent", "agree", "ahead", "alarm", "album", "alert", "alike", "alive", "allow", "alone", "along", "alter", "among", "anger", "angle", "angry", "apart", "apple", "apply", "arena", "argue", "arise", "array", "aside", "asset", "audio", "audit", "avoid", "award", "aware", "badly", "baker", "bases", "basic", "basis", "beach", "began", "begin", "begun", "being", "below", "bench", "billy", "birth", "black", "blame", "blind", "block", "blood", "board", "boost", "booth", "bound", "brain", "brand", "bread", "break", "breed", "brief", "bring", "broad", "broke", "brown", "build", "built", "buyer", "cable", "calif", "carry", "catch", "cause", "chain", "chair", "chart", "chase", "cheap", "check", "chest", "chief", "child", "china", "chose", "civil", "claim", "class", "clean", "clear", "click", "clock", "close", "coach", "coast", "could", "count", "court", "cover", "craft", "crash", "cream", "crime", "cross", "crowd", "crown", "curve", "cycle", "daily", "dance", "dated", "dealt", "death", "debut", "delay", "depth", "doing", "doubt", "dozen", "draft", "drama", "drawn", "dream", "dress", "drill", "drink", "drive", "drove", "dying", "eager", "early", "earth", "eight", "elite", "empty", "enemy", "enjoy", "enter", "entry", "equal", "error", "event", "every", "exact", "exist", "extra", "faith", "false", "fault", "fiber", "field", "fifth", "fifty", "fight", "final", "first", "fixed", "flash", "fleet", "floor", "fluid", "focus", "force", "forth", "forty", "forum", "found", "frame", "frank", "fraud", "fresh", "front", "fruit", "fully", "funny", "giant", "given", "glass", "globe", "going", "grace", "grade", "grand", "grant", "grass", "great", "green", "gross", "group", "grown", "guard", "guess", "guest", "guide", "happy", "harry", "heart", "heavy", "hence", "henry", "horse", "hotel", "house", "human", "ideal", "image", "index", "inner", "input", "issue", "japan", "jimmy", "joint", "jones", "judge", "known", "label", "large", "laser", "later", "laugh", "layer", "learn", "lease", "least", "leave", "legal", "level", "lewis", "light", "limit", "links", "lives", "local", "logic", "loose", "lower", "lucky", "lunch", "lying", "magic", "major", "maker", "march", "maria", "match", "maybe", "mayor", "meant", "media", "metal", "might", "minor", "minus", "mixed", "model", "money", "month", "moral", "motor", "mount", "mouse", "mouth", "movie", "music", "needs", "never", "newly", "night", "noise", "north", "noted", "novel", "nurse", "occur", "ocean", "offer", "often", "order", "other", "ought", "paint", "panel", "paper", "party", "peace", "peter", "phase", "phone", "photo", "piece", "pilot", "pitch", "place", "plain", "plane", "plant", "plate", "point", "pound", "power", "press", "price", "pride", "prime", "print", "prior", "prize", "proof", "proud", "prove", "queen", "quick", "quiet", "quite", "radio", "raise", "range", "rapid", "ratio", "reach", "ready", "refer", "right", "rival", "river", "robin", "roger", "roman", "rough", "round", "route", "royal", "rural", "scale", "scene", "scope", "score", "sense", "serve", "seven", "shall", "shape", "share", "sharp", "sheet", "shelf", "shell", "shift", "shirt", "shock", "shoot", "short", "shown", "sight", "since", "sixth", "sixty", "sized", "skill", "sleep", "slide", "small", "smart", "smile", "smith", "smoke", "solid", "solve", "sorry", "sound", "south", "space", "spare", "speak", "speed", "spend", "spent", "split", "spoke", "sport", "staff", "stage", "stake", "stand", "start", "state", "steam", "steel", "stick", "still", "stock", "stone", "stood", "store", "storm", "story", "strip", "stuck", "study", "stuff", "style", "sugar", "suite", "super", "sweet", "table", "taken", "taste", "taxes", "teach", "teeth", "terry", "texas", "thank", "theft", "their", "theme", "there", "these", "thick", "thing", "think", "third", "those", "three", "threw", "throw", "tight", "times", "tired", "title", "today", "topic", "total", "touch", "tough", "tower", "track", "trade", "train", "treat", "trend", "trial", "tried", "tries", "truck", "truly", "trust", "truth", "twice", "under", "undue", "union", "unity", "until", "upper", "upset", "urban", "usage", "usual", "valid", "value", "video", "virus", "visit", "vital", "voice", "waste", "watch", "water", "wheel", "where", "which", "while", "white", "whole", "whose", "woman", "women", "world", "worry", "worse", "worst", "worth", "would", "wound", "write", "wrong"]
 createMenu()
 let home=true
 
@@ -28,10 +20,9 @@ function createMenu(){
 results.innerHTML=""
 menu.innerHTML=`
 <h1><span id="spantitle">W</span>ORDLE</h1>
-<button id="dailybtn"><b>DIARIO</b></button>
-<button id="practicebtn"><b>PRACTICAR</b></button>
+<button id="practicebtn"><b>JUGAR</b></button>
 `
-document.getElementById("dailybtn").onclick=function(){
+document.getElementById("practicebtn").onclick=function(){
 home=false
 Gamestart(true)
 }
@@ -149,9 +140,10 @@ let maincheck=function(myword){
         if (round>6){
             return Reset(word,round)
         }
-        home=false
+        
         }
-}
+        home=false
+    }
 Validateword(getWord())
 
         function checkforgoodletters(){
@@ -247,6 +239,7 @@ datecondition=checkDate(myday[0],myday[1],myday[2])
 else{
 datecondition=2
 }
+datecondition=0
 if (datecondition==0){
     if (round<7){
 currentstreak++
@@ -257,7 +250,6 @@ if (currentstreak>maxstreak){
     currentstreak=0
 }
 }else if (datecondition==1){
-alert("wz")
 }else{
     if (round<7){
 currentstreak=1
@@ -368,7 +360,6 @@ let sumy=(m==12&&d==31)
 let nextday=d+sumd
 let nextmonth=m+summ
 let nextyear=y+sumy
-alert([nextday,nextmonth,nextyear,d,m,y])
 if (day==nextday&&month==nextmonth&&year==nextyear){
 return 0
 }else if (d==day&&m==nextmonth&&y==year){
